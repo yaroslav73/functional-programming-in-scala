c454e1893ec62b6629bbcbc3b6d7882bdb435b9d
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2019-05-21 19:35:14 +0300

    Implement getOrElse, orElse, filter functions

--- src/main/scala/chapter04/option/Option.scala
+++ src/main/scala/chapter04/option/Option.scala
@@ -6,18 +6,32 @@ sealed trait Option[+A] {
     case Some(value) => Some(f(value))
     case None => None
   }
+
   // Apply f, which may fail, to the Option if not None
   def flatMap[B](f: A => Option[B]): Option[B] = this match {
     case Some(value) => f(value)
     case None => None
   }
+
   // The B >: A says that B type parameter must be a supertype of A
-  def getOrElse[B >: A](default: => B): B = ???
+  def getOrElse[B >: A](default: => B): B = this match {
+    case Some(value) => value
+    case None => default
+  }
+
   // Don't evaluate ob unless needed
-  def orElse[B >: A](ob: Option[B]): Option[B] = ???
+  def orElse[B >: A](ob: Option[B]): Option[B] = this match {
+    case Some(value) => Some(value)
+    case None => ob
+  }
+
   // Convert Some to None if the value doesn't satisfy f
-  def filter(f: A => Boolean): Option[A] = ???
+  def filter(f: A => Boolean): Option[A] = this match {
+    case Some(value) if f(value) => Some(value)
+    case _ => None
+  }
 }
+
 case class Some[+A](value: A) extends Option[A]
 case object None extends Option[Nothing]
 
--- src/test/scala/chapter04/OptionSpec.scala
+++ src/test/scala/chapter04/OptionSpec.scala
@@ -32,5 +32,54 @@ class OptionSpec extends WordSpec {
         assert(none.flatMap(Some(_)) == None)
       }
     }
+
+    "apply getOrElse to Some('username')" should {
+      "should return 'username'" in {
+        val some: Option[String] = Some("admin")
+        assert(some.getOrElse("anonymous") == "admin")
+      }
+    }
+
+    "apply getOrElse to None" should {
+      "should return default value" in {
+        val none: Option[String] = None
+        assert(none.getOrElse("anonymous") == "anonymous")
+      }
+    }
+
+    "apply orElse to Some(13)" should {
+      "should return Some(13)" in {
+        val some: Option[Int] = Some(13)
+        assert(some.orElse(Some(0)) == Some(13))
+      }
+    }
+
+    "apply orElse to None" should {
+      "should evaluate default value" in {
+        val none: Option[Double] = None
+        assert(none.getOrElse(Some(2d * 7)) == Some(14d))
+      }
+    }
+
+    "apply filter to Some(13)" should {
+      "should return Some(13) if it's not even number" in {
+        val some: Option[Int] = Some(13)
+        assert(some.filter(_ % 2 != 0) == Some(13))
+      }
+    }
+
+    "apply filter to Some(Double.NegativeInfinity)" should {
+      "should return None if it's not positive infinity" in {
+        val some: Option[Double] = Some(Double.NegativeInfinity)
+        assert(some.filter(_.isPosInfinity) == None)
+      }
+    }
+
+    "apply filter to None" should {
+      "should return None" in {
+        val none: Option[Double] = None
+        assert(none.filter(_.isNaN) == None)
+      }
+    }
   }
 }

44c51722a9b97e48db5648f4982b3eb191f12779
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-02-25 23:06:59 +0200

    Add solution for what is monoid part.

---
+++ src/main/scala/chapter10/Monoid.scala
@@ -0,0 +1,60 @@
+package chapter10
+
+/*
+ * Monoid consists of the following:
+ * - some type A
+ * - an associative binary operation, op, that takes two values of typ A
+ *   and combines them into one: op(x, y), z) == op(x, op(y, z))
+ *   for any of x: A, y: A, z: A
+ * - a value, zero: A, that is an identity for that operation:
+ *   op(x, zero) == op(zero, x) == x for any x: A
+ *
+ * Last two point called the monoid laws.
+ */
+trait Monoid[A] {
+  def op(a1: A, a2: A): A
+  def zero: A
+}
+
+object Monoid {
+  val stringMonoid: Monoid[String] = new Monoid[String] {
+    override def op(a1: String, a2: String): String = a1 + a2
+    override def zero: String = ""
+  }
+
+  val intAddition: Monoid[Int] = new Monoid[Int] {
+    override def op(a1: Int, a2: Int): Int = a1 + a2
+    override def zero: Int = 0
+  }
+
+  val intMultiplication: Monoid[Int] = new Monoid[Int] {
+    override def op(a1: Int, a2: Int): Int = a1 * a2
+    override def zero: Int = 1
+  }
+
+  val booleanOr: Monoid[Boolean] = new Monoid[Boolean] {
+    override def op(a1: Boolean, a2: Boolean): Boolean = a1 || a2
+    override def zero: Boolean = false
+  }
+
+  val booleanAnd: Monoid[Boolean] = new Monoid[Boolean] {
+    override def op(a1: Boolean, a2: Boolean): Boolean = a1 && a2
+    override def zero: Boolean = true
+  }
+
+  def listMonoid[A]: Monoid[List[A]] = new Monoid[List[A]] {
+    override def op(a1: List[A], a2: List[A]): List[A] = a1 ++ a2
+    override def zero: List[A] = Nil
+  }
+
+  def optionMonoid[A: Monoid]: Monoid[Option[A]] = new Monoid[Option[A]] {
+    val instance: Monoid[A] = implicitly[Monoid[A]]
+    override def op(a1: Option[A], a2: Option[A]): Option[A] = a1.flatMap(v1 => a1.map(v2 => instance.op(v1, v2)))
+    override def zero: Option[A] = None
+  }
+
+  def endoMonoid[A]: Monoid[A => A] = new Monoid[A => A] {
+    override def op(a1: A => A, a2: A => A): A => A = a1 andThen a2
+    override def zero: A => A = (a: A) => a
+  }
+}

3338d7636928b9b14bd612bb78bb78d95a8a4473
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-09-08 23:00:49 +0300

    Make Tree subclasses final

--- src/main/scala/chapter03/datastructures/Tree.scala
+++ src/main/scala/chapter03/datastructures/Tree.scala
@@ -6,13 +6,13 @@ sealed trait Tree[+A] {
   def depth: Int
 }
 
-case class Leaf[A](value: A) extends Tree[A] {
+final case class Leaf[A](value: A) extends Tree[A] {
   override def size: Int = 1
 
   override def depth: Int = 1
 }
 
-case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A] {
+final case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A] {
   override def size: Int = left.size + right.size
 
   override def depth: Int = this match {

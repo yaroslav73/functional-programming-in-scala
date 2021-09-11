8ad98def9a6cce929d67035145b714ac27624cc3
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-09-11 13:03:59 +0300

    Update sequence and traverse implementation.

--- src/main/scala/chapter04/option/Option.scala
+++ src/main/scala/chapter04/option/Option.scala
@@ -2,19 +2,21 @@ package chapter04.option
 
 sealed trait Option[+A] {
   // Apply f if the option is not None
-  def map[B](f: A => B): Option[B] = this match {
-    case Some(value) => Some(f(value))
-    case None => None
-  }
+  def map[B](f: A => B): Option[B] =
+    this match {
+      case Some(value) => Some(f(value))
+      case None        => None
+    }
 
   // Apply f, which may fail, to the Option if not None
   def flatMap[B](f: A => Option[B]): Option[B] = map(f).getOrElse(None)
 
   // The B >: A says that B type parameter must be a supertype of A
-  def getOrElse[B >: A](default: => B): B = this match {
-    case Some(value) => value
-    case None => default
-  }
+  def getOrElse[B >: A](default: => B): B =
+    this match {
+      case Some(value) => value
+      case None        => default
+    }
 
   // Don't evaluate ob unless needed
   def orElse[B >: A](that: Option[B]): Option[B] = map(Some(_)).getOrElse(that)
@@ -32,22 +34,17 @@ case object None extends Option[Nothing]
 object Option {
   // Combine two Option values using binary function.
   // If either value is None, then return value is too.
-  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
-    a flatMap { i =>
-      b map { j => f(i, j) }
-    }
-  }
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a.flatMap(a => b.map(b => f(a, b)))
 
-  def sequence[A](l: List[Option[A]]): Option[List[A]] = {
-    l match {
-      case Nil => Some(List.empty[A])
-      case head :: tail => head flatMap { h => sequence(tail) map { list => h :: list } }
+  def sequence[A](xs: List[Option[A]]): Option[List[A]] =
+    xs match {
+      case Nil          => Some(Nil)
+      case head :: tail => head.flatMap(a => sequence(tail).map(list => a :: list))
     }
-  }
 
-  def traverse[A, B](l: List[A])(f: A => Option[B]): Option[List[B]] = {
-    sequence(l map f)
-  }
+  def traverse[A, B](xs: List[A])(f: A => Option[B]): Option[List[B]] =
+    xs.foldRight[Option[List[B]]](Some(Nil))((elem, init) => map2(f(elem), init)(_ :: _))
 
   def mean(xs: Seq[Double]): Option[Double] = if (xs.isEmpty) None else Some(xs.sum / xs.length)
 }

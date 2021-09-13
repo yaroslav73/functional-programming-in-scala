c336788f49fcf0581c409bc7ca5920e53a8630f6
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-09-14 00:25:30 +0300

    Add dirty implementation for hasSubsequence

--- src/main/scala/chapter03/datastructures/List.scala
+++ src/main/scala/chapter03/datastructures/List.scala
@@ -3,54 +3,61 @@ package chapter03.datastructures
 import scala.annotation.tailrec
 
 sealed trait List[+A] {
-  def setHead[B >: A](x: B): List[B] = this match {
-    case Nil => Cons(x, Nil)
-    case Cons(_, tail) => Cons(x, tail)
-  }
+  def setHead[B >: A](x: B): List[B] =
+    this match {
+      case Nil           => Cons(x, Nil)
+      case Cons(_, tail) => Cons(x, tail)
+    }
 
-  def tail: List[A] = this match {
-    case Nil => throw new UnsupportedOperationException("tail of empty list")
-    case Cons(_, tail) => tail
-  }
+  def tail: List[A] =
+    this match {
+      case Nil           => throw new UnsupportedOperationException("tail of empty list")
+      case Cons(_, tail) => tail
+    }
 
-  def drop(n: Int): List[A] = this match {
-    case Nil => Nil
-    case Cons(_, tail) if n > 0 => tail.drop(n - 1)
-    case _ => this
-  }
+  def drop(n: Int): List[A] =
+    this match {
+      case Nil                    => Nil
+      case Cons(_, tail) if n > 0 => tail.drop(n - 1)
+      case _                      => this
+    }
 
-  def dropWhile(p: A => Boolean): List[A] = this match {
-    case Nil => Nil
-    case Cons(head, tail) if p(head) => tail.dropWhile(p)
-    case _ => this
-  }
+  def dropWhile(p: A => Boolean): List[A] =
+    this match {
+      case Nil                         => Nil
+      case Cons(head, tail) if p(head) => tail.dropWhile(p)
+      case _                           => this
+    }
 
   def append[B >: A](that: List[B]): List[B] =
     foldRight(that)((elem, init) => Cons(elem, init))
 
-  def init: List[A] = this match {
-    case Nil => throw new UnsupportedOperationException("init of empty list")
-    case Cons(_, Nil) => Nil
-    case Cons(head, tail) => Cons(head, tail.init)
-  }
+  def init: List[A] =
+    this match {
+      case Nil              => throw new UnsupportedOperationException("init of empty list")
+      case Cons(_, Nil)     => Nil
+      case Cons(head, tail) => Cons(head, tail.init)
+    }
 
   def foldRight[B](init: B)(f: (A, B) => B): B =
     foldLeft((b: B) => b)((init, elem) => b => init(f(elem, b)))(init)
 
   @tailrec
-  final def foldLeft[B](init: B)(f: (B, A) => B): B = this match {
-    case Nil => init
-    case Cons(head, tail) => tail.foldLeft(f(init, head))(f)
-  }
+  final def foldLeft[B](init: B)(f: (B, A) => B): B =
+    this match {
+      case Nil              => init
+      case Cons(head, tail) => tail.foldLeft(f(init, head))(f)
+    }
 
   def length: Int = foldLeft(0)((init, _) => init + 1)
 
   def reverse: List[A] = foldLeft(Nil: List[A])((init, elem) => Cons(elem, init))
 
-  def map[B](f: A => B): List[B] = this match {
-    case Nil => Nil
-    case Cons(head, tail) => Cons(f(head), tail.map(f))
-  }
+  def map[B](f: A => B): List[B] =
+    this match {
+      case Nil              => Nil
+      case Cons(head, tail) => Cons(f(head), tail.map(f))
+    }
 
   def filter(p: A => Boolean): List[A] =
     flatMap(e => if (p(e)) List(e) else Nil)
@@ -58,13 +65,20 @@ sealed trait List[+A] {
   def flatMap[B](f: A => List[B]): List[B] =
     foldLeft(List.empty[B])((init, elem) => init.append(f(elem)))
 
-  def zipWith[B >: A](that: List[B])(f: (B, B) => B): List[B] = (this, that) match {
-    case (_, Nil) => Nil
-    case (Nil, _) => Nil
-    case (Cons(xh, xt), Cons(yh, yt)) => Cons(f(xh, yh), xt.zipWith(yt)(f))
-  }
+  def zipWith[B >: A](that: List[B])(f: (B, B) => B): List[B] =
+    (this, that) match {
+      case (_, Nil)                     => Nil
+      case (Nil, _)                     => Nil
+      case (Cons(xh, xt), Cons(yh, yt)) => Cons(f(xh, yh), xt.zipWith(yt)(f))
+    }
 
-  def hasSubsequence[B >: A](xs: List[B]): Boolean = ???
+  def hasSubsequence[B >: A](sub: List[B]): Boolean =
+    (this, sub) match {
+      case (Cons(ha, ta), Cons(hb, tb)) if ha == hb => ta.hasSubsequence(tb)
+      case (Cons(_, ta), _)                         => ta.hasSubsequence(sub)
+      case (_, Nil)                                 => true
+      case _                                        => false
+    }
 }
 
 case object Nil extends List[Nothing]
@@ -81,44 +95,51 @@ object List {
 
   def addOne(xs: List[Int]): List[Int] = {
     @tailrec
-    def loop(xs: List[Int], acc: List[Int]): List[Int] = xs match {
-      case Nil => acc
-      case Cons(head, tail) => loop(tail, Cons(head + 1, acc))
-    }
+    def loop(xs: List[Int], acc: List[Int]): List[Int] =
+      xs match {
+        case Nil              => acc
+        case Cons(head, tail) => loop(tail, Cons(head + 1, acc))
+      }
 
     loop(xs, Nil).reverse
   }
 
-  def addingElements(xsx: List[Int], xsy: List[Int]): List[Int] = xsx match {
-    case Nil => Nil
-    case Cons(xHead, xTail) => xsy match {
+  def addingElements(xsx: List[Int], xsy: List[Int]): List[Int] =
+    xsx match {
       case Nil => Nil
-      case Cons(yHead, yTail) => Cons(xHead + yHead, addingElements(xTail, yTail))
+      case Cons(xHead, xTail) =>
+        xsy match {
+          case Nil                => Nil
+          case Cons(yHead, yTail) => Cons(xHead + yHead, addingElements(xTail, yTail))
+        }
     }
-  }
 
   def concat[A](xs: List[List[A]]): List[A] =
     xs.foldLeft(List.empty[A])((init, elem) => init.append(elem))
 
-  def append[A](l1: List[A], l2: List[A]): List[A] = l2 match {
-    case Nil => l1
-    case Cons(h, t) => Cons(h, append(l1, t))
-  }
+  def append[A](l1: List[A], l2: List[A]): List[A] =
+    l2 match {
+      case Nil        => l1
+      case Cons(h, t) => Cons(h, append(l1, t))
+    }
 
-  def head[A](xs: List[A]): A = xs match {
-    case Nil => throw new NoSuchElementException("head of empty list")
-    case Cons(h, _) => h
-  }
+  def head[A](xs: List[A]): A =
+    xs match {
+      case Nil        => throw new NoSuchElementException("head of empty list")
+      case Cons(h, _) => h
+    }
 
-  def setHead[A](xs: List[A], x: A): List[A] = xs match {
-    case Nil => Cons(x, Nil)
-    case Cons(_, t) => Cons(x, t)
-  }
+  def setHead[A](xs: List[A], x: A): List[A] =
+    xs match {
+      case Nil        => Cons(x, Nil)
+      case Cons(_, t) => Cons(x, t)
+    }
 
-  def tail[A](xs: List[A]): List[A] = xs match {
-    case Nil => throw new UnsupportedOperationException("tail of empty list")
-    case Cons(_, t) => t
-  }
+  def tail[A](xs: List[A]): List[A] =
+    xs match {
+      case Nil        => throw new UnsupportedOperationException("tail of empty list")
+      case Cons(_, t) => t
+    }
 
   def init[A](xs: List[A]): List[A] = {
     def loop(xs: List[A], res: List[A]): List[A] = {
@@ -138,29 +159,32 @@ object List {
     else xs
   }
 
-  def dropWhile[A](xs: List[A])(p: A => Boolean): List[A] = xs match {
-    case Cons(h, t) if p(h) => dropWhile(t)(p)
-    case _ => xs
-  }
+  def dropWhile[A](xs: List[A])(p: A => Boolean): List[A] =
+    xs match {
+      case Cons(h, t) if p(h) => dropWhile(t)(p)
+      case _                  => xs
+    }
 
-  def foldRight[A, B](xs: List[A], init: B)(f: (A, B) => B): B = xs match {
-    case Nil => init
-    //    case Cons(h, t) => foldRight(t, f(h, init))(f)
-    case Cons(h, t) => f(h, foldRight(t, init)(f))
-  }
+  def foldRight[A, B](xs: List[A], init: B)(f: (A, B) => B): B =
+    xs match {
+      case Nil => init
+      //    case Cons(h, t) => foldRight(t, f(h, init))(f)
+      case Cons(h, t) => f(h, foldRight(t, init)(f))
+    }
 
   // TODO which different between right/left fold???
-  def foldLeft[A, B](xs: List[A], init: B)(f: (B, A) => B): B = xs match {
-    case Nil => init
-    case Cons(h, t) => foldLeft(t, f(init, h))(f)
-  }
+  def foldLeft[A, B](xs: List[A], init: B)(f: (B, A) => B): B =
+    xs match {
+      case Nil        => init
+      case Cons(h, t) => foldLeft(t, f(init, h))(f)
+    }
 
   def length[A](xs: List[A]): Int = foldRight(xs, 0)((_, a: Int) => a + 1)
 
   def reverse[A](xs: List[A]): List[A] = {
     def loop(xs: List[A], init: List[A]): List[A] = {
       xs match {
-        case Nil => init
+        case Nil        => init
         case Cons(h, t) => loop(t, Cons(h, init))
       }
     }
@@ -168,63 +192,73 @@ object List {
     loop(xs, Nil)
   }
 
-  def map[A, B](xs: List[A])(f: A => B): List[B] = xs match {
-    case Nil => Nil
-    case Cons(h, t) => Cons(f(h), map(t)(f))
-  }
+  def map[A, B](xs: List[A])(f: A => B): List[B] =
+    xs match {
+      case Nil        => Nil
+      case Cons(h, t) => Cons(f(h), map(t)(f))
+    }
 
-  def flatMap[A, B](xs: List[A])(f: A => List[B]): List[B] = xs match {
-    case Nil => Nil: List[B]
-    case Cons(h, t) => append(f(h), flatMap(t)(f))
-  }
+  def flatMap[A, B](xs: List[A])(f: A => List[B]): List[B] =
+    xs match {
+      case Nil        => Nil: List[B]
+      case Cons(h, t) => append(f(h), flatMap(t)(f))
+    }
 
-  def filter[A](xs: List[A])(f: A => Boolean): List[A] = xs match {
-    case Nil => xs
-    case Cons(h, t) if f(h) => Cons(h, filter(t)(f))
-    case Cons(_, t) => filter(t)(f)
-  }
+  def filter[A](xs: List[A])(f: A => Boolean): List[A] =
+    xs match {
+      case Nil                => xs
+      case Cons(h, t) if f(h) => Cons(h, filter(t)(f))
+      case Cons(_, t)         => filter(t)(f)
+    }
 
-  def zipWith[A](l1: List[A], l2: List[A])(f: (A, A) => A): List[A] = l1 match {
-    case Nil => Nil
-    case Cons(h, t) => Cons(f(h, head(l2)), zipWith(t, tail(l2))(f))
-  }
+  def zipWith[A](l1: List[A], l2: List[A])(f: (A, A) => A): List[A] =
+    l1 match {
+      case Nil        => Nil
+      case Cons(h, t) => Cons(f(h, head(l2)), zipWith(t, tail(l2))(f))
+    }
 
-  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = sub match {
-    case Nil => true
-    case Cons(h, t) if h == head(sup) => hasSubsequence(t, tail(sup))
-    case Cons(_, t) => hasSubsequence(sup, t)
-  }
+  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean =
+    sub match {
+      case Nil                          => true
+      case Cons(h, t) if h == head(sup) => hasSubsequence(t, tail(sup))
+      case Cons(_, t)                   => hasSubsequence(sup, t)
+    }
 
   def filterFlatMap[A](xs: List[A])(f: A => Boolean): List[A] =
     flatMap(xs)(h => if (f(h)) Cons(h, Nil) else Nil)
 
   def reverseFold[A](xs: List[A]): List[A] = foldLeft(xs, Nil: List[A])((init: List[A], x: A) => Cons(x, init))
 
-  def sum(ints: List[Int]): Int = ints match {
-    case Nil => 0
-    case Cons(h, t) => h + sum(t)
-  }
+  def sum(ints: List[Int]): Int =
+    ints match {
+      case Nil        => 0
+      case Cons(h, t) => h + sum(t)
+    }
 
   def addIntLists(l1: List[Int], l2: List[Int]): List[Int] = flatMap(l1)(a => map(l2)(b => a + b))
 
-  def double2String(nums: List[Double]): List[String] = nums match {
-    case Nil => Nil: List[String]
-    case Cons(h, t) => Cons(h.toString, double2String(t))
-  }
+  def double2String(nums: List[Double]): List[String] =
+    nums match {
+      case Nil        => Nil: List[String]
+      case Cons(h, t) => Cons(h.toString, double2String(t))
+    }
 
   def sumFoldLeft(ints: List[Int]): Int = foldLeft(ints, 0)(_ + _)
 
-  def product(nums: List[Double]): Double = nums match {
-    case Nil => 1.0
-    case Cons(h@0.0, _) => h
-    case Cons(h, t) => h * product(t)
-  }
+  def product(nums: List[Double]): Double =
+    nums match {
+      case Nil              => 1.0
+      case Cons(h @ 0.0, _) => h
+      case Cons(h, t)       => h * product(t)
+    }
 
-  def productFoldRight(xs: List[Double]): Double = xs.foldRight(1.0) { (elem, init) =>
-    if (elem == 0.0) 0.0 else elem * init
-  }
+  def productFoldRight(xs: List[Double]): Double =
+    xs.foldRight(1.0) { (elem, init) =>
+      if (elem == 0.0) 0.0 else elem * init
+    }
 
-  def productFoldLeft(xs: List[Double]): Double = xs.foldLeft(1.0) { (init, elem) =>
-    if (elem == 0.0) 0.0 else elem * init
-  }
+  def productFoldLeft(xs: List[Double]): Double =
+    xs.foldLeft(1.0) { (init, elem) =>
+      if (elem == 0.0) 0.0 else elem * init
+    }
 }

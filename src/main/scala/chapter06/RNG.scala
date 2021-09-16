5a9795f42314ea73e63d64fe5b6e6d9ff914992f
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-09-16 17:16:43 +0300

    Add SimpleRNG as case class to the RNG companion object.

--- src/main/scala/chapter06/RNG.scala
+++ src/main/scala/chapter06/RNG.scala
@@ -3,3 +3,14 @@ package chapter06
 trait RNG {
   def nextInt: (Int, RNG)
 }
+
+object RNG {
+  final case class SimpleRNG(seed: Long) extends RNG {
+    override def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0xdeece66dL + 0xbL) & 0xffffffffffffL
+      val nextRNG = SimpleRNG(newSeed)
+      val n = (newSeed >>> 16).toInt
+      (n, nextRNG)
+    }
+  }
+}

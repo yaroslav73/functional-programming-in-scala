71bb714bf1dcbe069f3a9161b3d49795df4d219d
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-09-18 20:09:40 +0300

    Implement map and map2 in terms of flatMap.

--- src/main/scala/chapter06/RNG.scala
+++ src/main/scala/chapter06/RNG.scala
@@ -25,17 +25,10 @@ object RNG {
     rng => (a, rng)
 
   def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
-    rng => {
-      val (a, ns) = s(rng)
-      (f(a), ns)
-    }
+    flatMap(s)(a => unit(f(a)))
 
   def map2[A, B, C](ra: Rand[A])(rb: Rand[B])(f: (A, B) => C): Rand[C] =
-    rng => {
-      val (a, r1) = ra(rng)
-      val (b, r2) = rb(r1)
-      (f(a, b), r2)
-    }
+    flatMap(ra)(a => map(rb)(b => f(a, b)))
 
   def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] =
     map2(ra)(rb)((_, _))

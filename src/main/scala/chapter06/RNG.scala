bd4e27c64550e707aded8699ad9c82e33b84c6d0
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-09-18 17:08:06 +0300

    Implement map2 and add method both in terms of map2.

--- src/main/scala/chapter06/RNG.scala
+++ src/main/scala/chapter06/RNG.scala
@@ -30,6 +30,16 @@ object RNG {
       (f(a), ns)
     }
 
+  def map2[A, B, C](ra: Rand[A])(rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    rng => {
+      val (a, r1) = ra(rng)
+      val (b, r2) = rb(r1)
+      (f(a, b), r2)
+    }
+
+  def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] =
+    map2(ra)(rb)((_, _))
+
   def nonNegativeEven: Rand[Int] = map(nonNegativeInt)(i => i - i % 2)
 
   def nonNegativeInt(rng: RNG): (Int, RNG) = {

cef93a91f617de8cdf818eb9b1205975de66dee9
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-10-10 17:15:53 +0300

    Update Prop design, add author implementation of forAll.

--- src/main/scala/chapter08/Prop.scala
+++ src/main/scala/chapter08/Prop.scala
@@ -1,15 +1,52 @@
 package chapter08
 
-import chapter08.Prop.{FailedCase, SuccessCount}
+import chapter06.RNG
+import chapter08.Prop.{Result, TestCases}
 
-trait Prop {
-  def check(): Either[(FailedCase, SuccessCount), SuccessCount]
+case class Prop(run: (TestCases, RNG) => Result) {
+  def check(): Result = ???
   def &&(p: Prop): Prop = ???
 }
 
 object Prop {
+  type TestCases = Int
   type FailedCase = String
   type SuccessCount = Int
 
-  def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
+  sealed trait Result {
+    def isFalsified: Boolean
+  }
+  case object Passed extends Result {
+    override def isFalsified: Boolean = true
+  }
+  case class Falsified(failure: FailedCase, successes: SuccessCount) extends Result {
+    override def isFalsified: Boolean = false
+  }
+
+  def forAll[A](gen: Gen[A])(f: A => Boolean): Prop =
+    Prop { (n, rng) =>
+      randomStream(gen)(rng)
+        .zip(LazyList.from(0))
+        .take(n)
+        .map {
+          case (a, i) =>
+            try {
+              if (f(a)) Passed else Falsified(a.toString, i)
+            } catch {
+              case e: Exception => Falsified(buildMessage(a, e), n)
+            }
+        }
+        .find(_.isFalsified)
+        .getOrElse(Passed)
+    }
+
+  def randomStream[A](g: Gen[A])(rng: RNG): LazyList[A] =
+    LazyList.unfold(rng)(rng => Some(g.sample.run(rng)))
+
+  def buildMessage[A](s: A, e: Exception): String =
+    s"""
+       |Test case: $s
+       |generated an exception: ${e.getMessage}
+       |stack trace: ${e.getStackTrace.mkString("\n")}
+       |""".stripMargin
 }

5d9f56b1fe05941092175e0f0b2b907865bf5c13
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-10-14 21:42:10 +0300

    Add infix syntax to parsers

--- src/main/scala/chapter09/Parsers.scala
+++ src/main/scala/chapter09/Parsers.scala
@@ -1,6 +1,19 @@
 package chapter09
 
-trait Parsers[ParseError, Parser[+_]] {
+import scala.language.implicitConversions
+
+trait Parsers[ParseError, Parser[+_]] { self =>
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
   def char(c: Char): Parser[Char]
+
+  def or[A](p1: Parser[A], p2: Parser[A]): Parser[A]
+
+  implicit def string(s: String): Parser[String]
+  implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps[A](p)
+  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
+
+  case class ParserOps[A](p: Parser[A]) {
+    def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+    def or[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+  }
 }

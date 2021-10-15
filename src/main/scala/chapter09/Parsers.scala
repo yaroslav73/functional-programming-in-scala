9991881a2f45e09fd49c860c674ecb1be1f9e0a1
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-10-15 18:42:45 +0300

    Add manyOne, product, slice, map2.

--- src/main/scala/chapter09/Parsers.scala
+++ src/main/scala/chapter09/Parsers.scala
@@ -15,7 +15,15 @@ trait Parsers[ParseError, Parser[+_]] { self =>
 
   def many[A](p: Parser[A]): Parser[List[A]]
 
+  def manyOne[A](p: Parser[A]): Parser[List[A]]
+
+  def product[A, B](p1: Parser[A], p2: Parser[B]): Parser[(A, B)]
+
+  def slice[A](p: Parser[A]): Parser[String]
+
   def map[A, B](p: Parser[A])(f: A => B): Parser[B]
+
+  def map2[A, B, C](p1: Parser[A], p2: Parser[B])(f: (A, B) => C): Parser[C]
 
   implicit def string(s: String): Parser[String]
   implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps[A](p)
@@ -24,7 +32,9 @@ trait Parsers[ParseError, Parser[+_]] { self =>
   case class ParserOps[A](p: Parser[A]) {
     def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
     def or[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+    def many: Parser[List[A]] = self.many(p)
     def map[B](f: A => B): Parser[B] = self.map(p)(f)
+    def **[B](p2: Parser[B]): Parser[(A, B)] = self.product(p, p2)
   }
 
   object Laws {

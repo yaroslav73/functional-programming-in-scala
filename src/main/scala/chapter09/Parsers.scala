71745edd5f658c51901eecfc710f5f0b074ec4fb
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-10-15 18:11:08 +0300

    Add many, char and succeed.

--- src/main/scala/chapter09/Parsers.scala
+++ src/main/scala/chapter09/Parsers.scala
@@ -6,9 +6,14 @@ import scala.language.implicitConversions
 
 trait Parsers[ParseError, Parser[+_]] { self =>
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
-  def char(c: Char): Parser[Char]
+
+  def char(c: Char): Parser[Char] = string(c.toString) map (_.charAt(0))
+
+  def succeed[A](a: A): Parser[A] = string("") map (_ => a)
 
   def or[A](p1: Parser[A], p2: Parser[A]): Parser[A]
+
+  def many[A](p: Parser[A]): Parser[List[A]]
 
   def map[A, B](p: Parser[A])(f: A => B): Parser[B]
 

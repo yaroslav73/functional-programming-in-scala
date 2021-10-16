f29b37dd50480540a2e29df5e141baf2f5af3ec6
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-10-16 11:51:31 +0300

    Add flatMap combinator. Implement map, map2 and product in terms of flatMap.

--- src/main/scala/chapter09/Parsers.scala
+++ src/main/scala/chapter09/Parsers.scala
@@ -3,6 +3,7 @@ package chapter09
 import chapter08.{Gen, Prop}
 
 import scala.language.implicitConversions
+import scala.util.matching.Regex
 
 trait Parsers[ParseError, Parser[+_]] { self =>
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
@@ -21,14 +22,17 @@ trait Parsers[ParseError, Parser[+_]] { self =>
     if (n <= 0) succeed(List.empty[A])
     else map2(p, listOfN(n - 1, p))(_ :: _)
 
-  def product[A, B](p1: Parser[A], p2: => Parser[B]): Parser[(A, B)]
+  def product[A, B](p1: Parser[A], p2: => Parser[B]): Parser[(A, B)] = p1.flatMap(a => p2.map(b => (a, b)))
 
   def slice[A](p: Parser[A]): Parser[String]
 
-  def map[A, B](p: Parser[A])(f: A => B): Parser[B]
+  def map[A, B](p: Parser[A])(f: A => B): Parser[B] = flatMap(p)(a => succeed(f(a)))
 
-  def map2[A, B, C](p1: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] = product(p1, p2).map(f.tupled)
+  def map2[A, B, C](p1: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] = p1.flatMap(a => p2.map(b => f(a, b)))
 
+  def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]
+
+  implicit def regex(r: Regex): Parser[String]
   implicit def string(s: String): Parser[String]
   implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps[A](p)
   implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
@@ -39,6 +43,7 @@ trait Parsers[ParseError, Parser[+_]] { self =>
     def many: Parser[List[A]] = self.many(p)
     def map[B](f: A => B): Parser[B] = self.map(p)(f)
     def **[B](p2: Parser[B]): Parser[(A, B)] = self.product(p, p2)
+    def flatMap[B](f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
   }
 
   object Laws {

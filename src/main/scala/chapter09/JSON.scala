57c9e261c2131704105d0190e905f0486dd470d3
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-10-16 22:08:57 +0300

    Add json data type. And json example for future.

---
+++ src/main/scala/chapter09/JSON.scala
@@ -0,0 +1,58 @@
+package chapter09
+
+trait JSON
+object JSON {
+  case object JsNull extends JSON
+  case class JsString(value: String) extends JSON
+  case class JsNumber(value: Double) extends JSON
+  case class JsBoolean(value: Boolean) extends JSON
+  case class JsArray(value: IndexedSeq[JSON]) extends JSON
+  case class JsObject(value: Map[String, JSON]) extends JSON
+
+  def jsonParser[ParseError, Parser[+_]](P: Parsers[ParseError, Parser]): Parser[JSON] = {
+    import P.{string => _, _}
+    ???
+  }
+
+  /**
+    * JSON parsing example.
+    */
+  object JSONExample extends App {
+    val jsonTxt = """
+      {
+        "Company name" : "Microsoft Corporation",
+        "Ticker"  : "MSFT",
+        "Active"  : true,
+        "Price"   : 30.66,
+        "Shares outstanding" : 8.38e9,
+        "Related companies" : [ "HPQ", "IBM", "YHOO", "DELL", "GOOG" ]
+      }
+      """
+
+    val malformedJson1 = """
+      {
+        "Company name" ; "Microsoft Corporation"
+      }
+      """
+
+    val malformedJson2 = """
+      [
+        [ "HPQ", "IBM",
+        "YHOO", "DELL" ++
+        "GOOG"
+        ]
+      ]
+      """
+
+    //  val P = Reference
+    //  import ReferenceTypes.Parser
+    //  def printResult[E](e: Either[E, JSON]) =
+    //    e.fold(println, println)
+    //  val json: Parser[JSON] = JSON.jsonParser(P)
+    //  printResult { P.run(json)(jsonTxt) }
+    //  println("--")
+    //  printResult { P.run(json)(malformedJson1) }
+    //  println("--")
+    //  printResult { P.run(json)(malformedJson2) }
+  }
+}

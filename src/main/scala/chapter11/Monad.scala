a8298989e63eed0777a182fa765e7a28502c9deb
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-10-22 00:03:26 +0300

    Add Monad trait.

---
+++ src/main/scala/chapter11/Monad.scala
@@ -0,0 +1,15 @@
+package chapter11
+
+trait Monad[F[_]] extends Functor[F] {
+  def unit[A](a: => A): F[A]
+  def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
+
+  def map[A, B](fa: F[A])(f: A => B): F[B] =
+    flatMap(fa)(a => unit(f(a)))
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+    flatMap(fa)(a => map(fb)(b => f(a, b)))
+}
+
+object Monad {
+
+}

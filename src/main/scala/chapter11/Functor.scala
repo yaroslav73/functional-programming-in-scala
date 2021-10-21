53e3fcaa23e2363f7cb186321013965c585507e9
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-10-22 00:00:00 +0300

    Add functor trait and instance for List.

---
+++ src/main/scala/chapter11/Functor.scala
@@ -0,0 +1,20 @@
+package chapter11
+
+trait Functor[F[_]] {
+  def map[A, B](fa: F[A])(f: A => B): F[B]
+
+  def distribute[A, B](fab: F[(A, B)]): (F[A], F[B]) =
+    (map(fab) { case (a, _) => a }, map(fab) { case (_, b) => b })
+
+  def codistribute[A, B](e: Either[F[A], F[B]]): F[Either[A, B]] =
+    e match {
+      case Left(fa)  => map(fa)(Left(_))
+      case Right(fb) => map(fb)(Right(_))
+    }
+}
+
+object Functor {
+  val listFunctor: Functor[List] = new Functor[List] {
+    override def map[A, B](fa: List[A])(f: A => B): List[B] = fa.map(f)
+  }
+}

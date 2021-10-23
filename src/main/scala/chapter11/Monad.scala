94be068e078a7ce5872723a6f1f42ea82a6a1a71
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-10-23 23:02:55 +0300

    Add comment and add solution from author for traverse.

--- src/main/scala/chapter11/Monad.scala
+++ src/main/scala/chapter11/Monad.scala
@@ -9,11 +9,12 @@ trait Monad[F[_]] extends Functor[F] {
   def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
     flatMap(fa)(a => map(fb)(b => f(a, b)))
 
+  // TODO: notice flatMap inside foldLeft - it's looks like map2 :)
   def sequence[A](lfa: List[F[A]]): F[List[A]] =
     lfa.foldLeft(unit(List.empty[A]))((acc, fa) => flatMap(fa)(a => map(acc)(list => list :+ a)))
 
   def traverse[A, B](la: List[A])(f: A => F[B]): F[List[B]] =
-    sequence(la.map(f))
+    la.foldRight(unit(List.empty[B]))((a, acc) => map2(f(a), acc)((b, lb) => b :: lb))
 }
 
 object Monad {

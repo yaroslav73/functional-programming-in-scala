68dd62ea84b289f28532fe145dbadb34e6eac4cb
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-10-24 23:15:01 +0300

    Add product function.

--- src/main/scala/chapter11/Monad.scala
+++ src/main/scala/chapter11/Monad.scala
@@ -8,17 +8,22 @@ trait Monad[F[_]] extends Functor[F] {
 
   def map[A, B](fa: F[A])(f: A => B): F[B] =
     flatMap(fa)(a => unit(f(a)))
+
   def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
     flatMap(fa)(a => map(fb)(b => f(a, b)))
 
   // TODO: notice flatMap inside foldLeft - it's looks like map2 :)
   def sequence[A](lfa: List[F[A]]): F[List[A]] =
     lfa.foldLeft(unit(List.empty[A]))((acc, fa) => flatMap(fa)(a => map(acc)(list => list :+ a)))
+
   def traverse[A, B](la: List[A])(f: A => F[B]): F[List[B]] =
     la.foldRight(unit(List.empty[B]))((a, acc) => map2(f(a), acc)((b, lb) => b :: lb))
 
   def replicateM[A](n: Int, fa: F[A]): F[List[A]] =
     sequence(List.fill(n)(fa))
+
+  def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] =
+    map2(fa, fb)((a, b) => a -> b)
 }
 
 object Monad {

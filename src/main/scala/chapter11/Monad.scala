252f0b5658117a8db9e651137bed78f3682427a0
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-10-25 23:43:04 +0300

    Add join implementations in terms of flatMap.

--- src/main/scala/chapter11/Monad.scala
+++ src/main/scala/chapter11/Monad.scala
@@ -4,10 +4,12 @@ import chapter06.State
 
 trait Monad[F[_]] extends Functor[F] {
   def unit[A](a: => A): F[A]
-  def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
+  def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B] // TODO: compose((_: Unit) => fa, f)(())
 
   def map[A, B](fa: F[A])(f: A => B): F[B] =
     flatMap(fa)(a => unit(f(a)))
+
+  def join[A](ffa: F[F[A]]): F[A] = flatMap(ffa)(fa => fa)
 
   def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
     flatMap(fa)(a => map(fb)(b => f(a, b)))

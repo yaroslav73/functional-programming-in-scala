7c16351ac96a7c1bc3495e86606a433cebe6900a
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-10-30 12:55:10 +0300

    Add Validation implementation.

---
+++ src/main/scala/chapter12/Validation.scala
@@ -0,0 +1,21 @@
+package chapter12
+
+sealed trait Validation[+E, +A]
+object Validation {
+  final case class Failure[E](head: E, tail: Vector[E] = Vector.empty[E]) extends Validation[E, Nothing]
+  final case class Success[A](value: A) extends Validation[Nothing, A]
+
+  def applicativeValidation[E]: Applicative[({ type f[x] = Validation[E, x] })#f] =
+    new Applicative[({ type f[x] = Validation[E, x] })#f] {
+      def unit[A](a: => A): Validation[E, A] = Success(a)
+
+      override def map2[A, B, C](fa: Validation[E, A], fb: Validation[E, B])(f: (A, B) => C): Validation[E, C] = {
+        (fa, fb) match {
+          case (Success(a), Success(b))           => Success(f(a, b))
+          case (f @ Failure(_, _), _)             => f
+          case (_, f @ Failure(_, _))             => f
+          case (Failure(ah, at), Failure(bh, bt)) => Failure(ah, (bh +: bt) ++ at)
+        }
+      }
+    }
+}

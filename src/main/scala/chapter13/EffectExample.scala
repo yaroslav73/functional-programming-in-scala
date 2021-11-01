8dc23f9917efc331783ee95c3fd9a56f0f4cd7c7
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-11-01 21:29:17 +0200

    Refactor contest method - extract winner evaluation to separate method.

--- src/main/scala/chapter13/EffectExample.scala
+++ src/main/scala/chapter13/EffectExample.scala
@@ -3,9 +3,14 @@ package chapter13
 object EffectExample extends App {
   final case class Player(name: String, score: Int)
 
-  def contest(p1: Player, p2: Player): Unit = {
-    if (p1.score > p2.score) println(s"${p1.name} is the winner!")
-    else if (p2.score > p1.score) println(s"${p2.name} is the winner!")
-    else println("It's a draw!")
-  }
+  def winner(p1: Player, p2: Player): Option[Player] =
+    if (p1.score > p2.score) Some(p1)
+    else if (p2.score > p1.score) Some(p2)
+    else None
+
+  def contest(p: Option[Player]): Unit =
+    p match {
+      case Some(Player(name, _)) => println(s"$name is the winner!")
+      case None                  => println("It's a draw!")
+    }
 }

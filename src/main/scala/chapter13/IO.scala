7e443294d90ca87e137e8bdb9b478cd34a1a7245
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-11-02 23:24:40 +0200

    Update IO implementation.

--- src/main/scala/chapter13/IO.scala
+++ src/main/scala/chapter13/IO.scala
@@ -1,5 +1,13 @@
 package chapter13
 
-trait IO {
+trait IO { self =>
   def run(): Unit
+  def ++(io: IO): IO = () => {
+    self.run()
+    io.run()
+  }
+}
+
+object IO {
+  def empty: IO = () => ()
 }

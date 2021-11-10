21ccc2cf92311b9551ec79393b94f4f424ef9807
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-11-10 17:38:50 +0200

    Add new data type ST known as state thread or state transition or state toke or state tag.

---
+++ src/main/scala/chapter14/ST.scala
@@ -0,0 +1,24 @@
+package chapter14
+
+trait ST[S, A] { self =>
+  protected def run(s: S): (A, S)
+
+  def map[B](f: A => B): ST[S, B] =
+    (s: S) => {
+      val (a, newS) = self.run(s)
+      (f(a), newS)
+    }
+
+  def flatMap[B](f: A => ST[S, B]): ST[S, B] =
+    (s: S) => {
+      val (a, newS) = self.run(s)
+      f(a).run(newS)
+    }
+}
+
+object ST {
+  def apply[S, A](a: => A): ST[S, A] = {
+    lazy val cache = a
+    (s: S) => (cache, s)
+  }
+}

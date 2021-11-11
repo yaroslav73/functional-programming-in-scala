2a31993d78a80059972d3559d968107b6e5bb95b
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-11-11 20:32:08 +0200

    Add solution for qs and use author solution for partition.

--- src/main/scala/chapter14/QuickSortExampleApp.scala
+++ src/main/scala/chapter14/QuickSortExampleApp.scala
@@ -35,4 +35,54 @@ object QuickSortExampleApp extends App {
     }
 
   println(quicksort(List(7, 2, 5, 3, 1, 4, 8, 6, 10, 9)))
+
+  def noop[S]: ST[S, Unit] = ST[S, Unit](())
+
+  def partition[S](a: STArray[S, Int], l: Int, r: Int, pivot: Int): ST[S, Int] = {
+    for {
+      pivotValue <- a.read(pivot)
+      _ <- a.swap(pivot, r)
+      j <- STRef(l)
+      _ <- (l until r).foldLeft(noop[S])((s, i) =>
+        for {
+          _ <- s
+          iValue <- a.read(i)
+          _ <-
+            if (iValue < pivotValue) for {
+              jValue <- j.read
+              _ <- a.swap(i, jValue)
+              _ <- j.write(jValue + 1)
+            } yield ()
+            else noop[S]
+        } yield ()
+      )
+      x <- j.read
+      _ <- a.swap(x, r)
+    } yield x
+  }
+
+  def qs[S](a: STArray[S, Int], l: Int, r: Int): ST[S, Unit] =
+    if (l < r) for {
+      pi <- partition(a, l, r, l + (r - l) / 2)
+      _ <- qs(a, l, pi - 1)
+      _ <- qs(a, pi + 1, r)
+    } yield ()
+    else noop
+
+  def pureQuicksort(xs: List[Int]): List[Int] = {
+    if (xs.isEmpty) xs
+    else {
+      ST.runST(new RunnableST[List[Int]] {
+        def apply[S]: ST[S, List[Int]] =
+          for {
+            arr <- STArray.fromList(xs)
+            size <- arr.size
+            _ <- qs(arr, 0, size - 1)
+            sorted <- arr.freeze
+          } yield sorted
+      })
+    }
+  }
+
+  println(pureQuicksort(List(7, 2, 5, 3, 1, 4, 8, 6, 10, 9)))
 }

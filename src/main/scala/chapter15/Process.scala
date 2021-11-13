f35527ff4bf8668ed4a9a49becb49a1e43ae9103
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-11-13 22:17:25 +0200

    Add apply method for Process.

--- src/main/scala/chapter15/Process.scala
+++ src/main/scala/chapter15/Process.scala
@@ -1,6 +1,17 @@
 package chapter15
 
-sealed trait Process[I, O]
+sealed trait Process[I, O] {
+  def apply(in: LazyList[I]): LazyList[O] =
+    this match {
+      case Halt() => LazyList.empty[O]
+      case Await(recovery) =>
+        in match {
+          case head #:: tail => recovery(Some(head))(tail)
+          case xs            => recovery(None)(xs)
+        }
+      case Emit(head, tail) => head #:: tail(in)
+    }
+}
 
 case class Emit[I, O](head: O, tail: Process[I, O] = Halt[I, O]()) extends Process[I, O]
 case class Await[I, O](recovery: Option[I] => Process[I, O]) extends Process[I, O]

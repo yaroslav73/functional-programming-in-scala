88ce8636338d71dc531087f65be80266c7fef070
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-11-21 19:51:20 +0200

    Add helper function Try and await.

--- src/main/scala/chapter15/process_with_f/Process.scala
+++ src/main/scala/chapter15/process_with_f/Process.scala
@@ -20,14 +20,24 @@ trait Process[F[_], O] {
 object Process {
   // The recovery function now takes an Either so we can handle errors.
   final case class Await[F[_], A, O](request: F[A], recovery: Either[Throwable, A] => Process[F, O]) extends Process[F, O]
+
   final case class Emit[F[_], O](head: O, tail: Process[F, O]) extends Process[F, O]
+
   // Halt due to err, which could be an actual error or End indicating normal termination
   final case class Halt[F[_], O](error: Throwable) extends Process[F, O]
 
   // An Exception that indicates normal termination.
   // This allows us to use Scala’s exception mechanism for control flow.
   case object End extends Exception
+
   // An Exception that indicates forceful termination.
   // We’ll see how this is used later.
   case object Kill extends Exception
+
+  def Try[F[_], O](p: Process[F, O]): Process[F, O] =
+    try p
+    catch { case e: Throwable => Halt(e) }
+
+  def await[F[_], A, O](request: F[A])(recovery: Either[Throwable, A] => Process[F, O]): Process[F, O] =
+    Await(request, recovery)
 }
